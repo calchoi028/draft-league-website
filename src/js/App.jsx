13dfd444f0248650759c55de0bba2086c1b060ee
Aquila <calchoi028@gmail.com>
2021-07-17 16:17:25 -0600

    refactor: organized files into folders

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from 'src/images/logo.svg';
+import 'src/css/App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
