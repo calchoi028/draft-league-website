9f2e794e3096eec18c0a5eb632776b94c0389891
Aquila <calchoi028@gmail.com>
2021-07-20 23:15:39 -0600

    refactor: added development pointers for home page

---
+++ src/constants/HomePageConstants.js
@@ -0,0 +1,4 @@
+// TODO: Add all the text for the home page here as constants. Try to create a
+// registry (map) to associate all the text with an id.
+
+export const HELLO_WORLD_CONSTANT = "Hello World";
